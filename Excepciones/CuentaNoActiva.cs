a240505106cfc3a5cfd45d7b7ada0faa934324e6
Josue Rueda <josuealexandro2013@gmail.com>
2025-05-02 18:20:30 -0300

    Modificados CuentaBancaria.cs, y añadidos CajaDeAhorro.cs, CuentaCorriente.cs, y la carpeta Excepciones con clases personalizadas

---
+++ Excepciones/CuentaNoActiva.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Excepciones
+{
+    public class CuentaNoActivaException : Exception
+    {
+        public CuentaNoActivaException(string estado) : base($"no se puede operar con la cuenta {estado}.")
+        {
+
+        }
+    }
+}
